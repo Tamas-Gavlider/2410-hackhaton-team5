632d610623cc952a97343c34878b1efe90f8a1ab
Mykola Kuievda <dankraig23@gmail.com>
2024-10-13 17:09:37 +0100

    Set .env secret key and debug mode, update settings

--- job_app_tracker/settings.py
+++ job_app_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -23,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qprmz65&qr2)xa#6ru20=3nhivb7!o6g9a5w1kx(*-8e!fmz86'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
