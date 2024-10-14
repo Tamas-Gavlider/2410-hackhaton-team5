522b869a724a4ac4d03d116f1bd5174f3d30329d
Mykola Kuievda <dankraig23@gmail.com>
2024-10-14 15:55:15 +0100

    Adjust settings for crispy, add urls and view for edit-application

--- job_app_tracker/settings.py
+++ job_app_tracker/settings.py
@@ -55,7 +55,12 @@ INSTALLED_APPS = [
     # Third party apps
     'allauth',
     'allauth.account',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- job_applications/views.py
+++ job_applications/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from .models import JobApplication
+from .forms import JobApplicationForm
 
 
-def dashboard(request):
-    return render(request, 'job_applications/dashboard.html')
+@login_required
+def edit_application(request, job_id):
+
+    job = get_object_or_404(JobApplication, pk=job_id, user=request.user)
+
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Temporarily until dashboard is ready !!!
+    else:
+        form = JobApplicationForm(instance=job)
+
+    context = {
+        'job': job,
+        'form': form,
+    }
+
+    return render(request, 'job_applications/edit-application.html', context)
