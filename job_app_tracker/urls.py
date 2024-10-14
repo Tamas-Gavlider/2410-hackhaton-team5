3c35d897e964285ecd758306ae9e63e9410d4a34
Viola Bergere <viola.bergere@gmail.com>
2024-10-14 16:47:02 +0000

    fix: Create error pages and add necessary URLs; styling to be implemented later

--- job_app_tracker/urls.py
+++ job_app_tracker/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . views import error_403, error_404, error_500
+
+
+handler403 = error_403
+handler404 = error_404
+handler500 = error_500
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
