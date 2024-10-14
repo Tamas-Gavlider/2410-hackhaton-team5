e3876a0d71964c8f5fdec3bdf7f79e12c489517f
Viola Bergere <viola.bergere@gmail.com>
2024-10-14 17:01:58 +0000

    feat: create error, FAQ and privacy templates, update URLs, and register FAQ model in admin panel

--- home/views.py
+++ home/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import FAQ
 
 
 def home(request):
     return render(request, 'home/index.html')
+
+
+def faq(request):
+    """ A view to return the FAQ page"""
+    faqs = FAQ.objects.all()
+
+    return render(request, "home/faq.html", {'faqs': faqs})
+
+
+def privacy_policy(request):
+    """ A view to return the Privacy Policy page"""
+
+    return render(request, "home/privacy_policy.html")
